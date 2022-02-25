e1e2b86015c4d31f525fa610cd50748124bf9fab
Mehdy Faik <faik.mehdy@gmail.com>
2022-02-25 10:12:10 -0700

    incremental advancement towards refactor into separate files

---
+++ tcp-client/spawn.c
@@ -0,0 +1,168 @@
+#include "spawn.h"
+
+static pthread_mutex_t gl_lock_connect;
+
+int32_t spawn_and_connect
+  ( const char* arg_local_ipv4
+  , uint16_t arg_local_port
+  , const char* arg_remote_ipv4
+  , uint16_t arg_remote_port
+  , struct sockaddr_in* rtn_local_sa
+  , struct sockaddr_in* rtn_remote_sa
+  )
+{
+  // arg check.
+  //
+  if ( (rtn_local_sa == NULL) || (rtn_remote_sa == NULL) ) { return -1; }
+
+  // socket().
+  // 
+  int32_t rtn_local_fd = socket(AF_INET, SOCK_STREAM, 0);
+  if (rtn_local_fd < 0)
+  {
+    if (gl_verbose_error)
+    {
+      printf("socket: error '%d', errno '%d' ('%s')\n"
+      , rtn_local_fd
+      , errno
+      , strerror(errno)
+      );
+    }
+    return rtn_local_fd;
+  }
+
+  // setsockopt() to allow reuse.
+  // 
+  int32_t reuse_enable = 1;
+  setsockopt(rtn_local_fd, SOL_SOCKET, SO_REUSEADDR, &reuse_enable, sizeof(reuse_enable));
+  setsockopt(rtn_local_fd, SOL_SOCKET, SO_REUSEPORT, &reuse_enable, sizeof(reuse_enable));
+
+  // bind() to the local.
+  //
+  rtn_local_sa->sin_family = AF_INET;
+  inet_pton( AF_INET, arg_local_ipv4, &(rtn_local_sa->sin_addr.s_addr) );
+  rtn_local_sa->sin_port = htons(arg_local_port);
+
+  int32_t error_bind = bind(rtn_local_fd, (struct sockaddr*)(rtn_local_sa), sizeof(*rtn_local_sa));
+  if (error_bind < 0)
+  {
+    if (gl_verbose_error)
+    {
+      printf("bind: error '%d', errno '%d' ('%s')\n"
+      , error_bind
+      , errno
+      , strerror(errno)
+      );
+    }
+    return error_bind;
+  }
+
+  // connect() to the remote.
+  //
+  rtn_remote_sa->sin_family = AF_INET;
+  inet_pton( AF_INET, arg_remote_ipv4, &(rtn_remote_sa->sin_addr.s_addr) );
+  rtn_remote_sa->sin_port = htons(arg_remote_port);
+
+  int32_t error_connect = connect(rtn_local_fd, (struct sockaddr*)(rtn_remote_sa), sizeof(*rtn_remote_sa));
+  if (error_connect < 0)
+  {
+    if (gl_verbose_error)
+    {
+      printf("connect: error '%d', errno '%d' ('%s')\n"
+      , error_connect
+      , errno
+      , strerror(errno)
+      );
+    }
+    return error_connect;
+  }
+
+  // ret.
+  return rtn_local_fd;
+}
+
+int32_t spawn_and_connect_lock
+  ( const char* arg_local_ipv4
+  , uint16_t arg_local_port
+  , const char* arg_remote_ipv4
+  , uint16_t arg_remote_port
+  , struct sockaddr_in* rtn_local_sa
+  , struct sockaddr_in* rtn_remote_sa
+  )
+{
+  // arg check.
+  //
+  if ( (rtn_local_sa == NULL) || (rtn_remote_sa == NULL) ) { return -1; }
+
+  // socket().
+  // 
+  int32_t rtn_local_fd = socket(AF_INET, SOCK_STREAM, 0);
+  if (rtn_local_fd < 0)
+  {
+    if (gl_verbose_error)
+    {
+      printf("socket: error '%d', errno '%d' ('%s')\n"
+      , rtn_local_fd
+      , errno
+      , strerror(errno)
+      );
+    }
+    return rtn_local_fd;
+  }
+
+  // setsockopt() to allow reuse.
+  // 
+  int32_t reuse_enable = 1;
+  setsockopt(rtn_local_fd, SOL_SOCKET, SO_REUSEADDR, &reuse_enable, sizeof(reuse_enable));
+  setsockopt(rtn_local_fd, SOL_SOCKET, SO_REUSEPORT, &reuse_enable, sizeof(reuse_enable));
+
+  // bind() to the local.
+  //
+  rtn_local_sa->sin_family = AF_INET;
+  inet_pton( AF_INET, arg_local_ipv4, &(rtn_local_sa->sin_addr.s_addr) );
+  rtn_local_sa->sin_port = htons(arg_local_port);
+
+  int32_t error_bind = bind(rtn_local_fd, (struct sockaddr*)(rtn_local_sa), sizeof(*rtn_local_sa));
+  if (error_bind < 0)
+  {
+    if (gl_verbose_error)
+    {
+      printf("bind: error '%d', errno '%d' ('%s')\n"
+      , error_bind
+      , errno
+      , strerror(errno)
+      );
+    }
+    return error_bind;
+  }
+
+  // lock connect().
+  pthread_mutex_lock(&gl_lock_connect);
+
+  // connect() to the remote.
+  //
+  rtn_remote_sa->sin_family = AF_INET;
+  inet_pton( AF_INET, arg_remote_ipv4, &(rtn_remote_sa->sin_addr.s_addr) );
+  rtn_remote_sa->sin_port = htons(arg_remote_port);
+
+  int32_t error_connect = connect(rtn_local_fd, (struct sockaddr*)(rtn_remote_sa), sizeof(*rtn_remote_sa));
+  if (error_connect < 0)
+  {
+    if (gl_verbose_error)
+    {
+      printf("connect: error '%d', errno '%d' ('%s')\n"
+      , error_connect
+      , errno
+      , strerror(errno)
+      );
+    }
+    return error_connect;
+  }
+
+  // unlock connect().
+  pthread_mutex_unlock(&gl_lock_connect);
+
+  // ret.
+  return rtn_local_fd;
+}
+
---
+++ tcp-client/spawn.h
@@ -0,0 +1,83 @@
+#ifndef SPAWN_H
+#define SPAWN_H
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <arpa/inet.h>
+#include <pthread.h>
+
+/**
+ * spawn_and_connect
+ * 
+ * @summary
+ *   Spawns a TCP client, and binds the client to 'arg_local_ipv4':'arg_local_port'.
+ *   Connects the TCP client to the remote endpoint 'arg_remote_ipv4':'arg_remote_port'.
+ *
+ * @return
+ *   The 'struct sockaddr_in' of the local TCP client [return parameter]
+ *   The 'struct sockaddr_in' of the remote TCP client [return parameter]
+ *   The socket file descriptor of the local TCP client [return value]
+ *
+ */
+int32_t spawn_and_connect
+  ( const char* arg_local_ipv4
+  , uint16_t arg_local_port
+  , const char* arg_remote_ipv4
+  , uint16_t arg_remote_port
+  , struct sockaddr_in* rtn_local_sa
+  , struct sockaddr_in* rtn_remote_sa
+  );
+
+/**
+ * spawn_and_connect_lock
+ *
+ * @summary
+ *   `spawn_and_connect`, but places a lock around socket calls.
+ *
+ * @return
+ *   ref. `spawn_and_connect`.
+ *
+ */
+int32_t spawn_and_connect_lock
+  ( const char* arg_local_ipv4
+  , uint16_t arg_local_port
+  , const char* arg_remote_ipv4
+  , uint16_t arg_remote_port
+  , struct sockaddr_in* rtn_local_sa
+  , struct sockaddr_in* rtn_remote_sa
+  );
+
+/**
+ * spawn_and_connect_args
+ * @summary
+ *   Represents wrapper for arguments to `spawn_and_connect`.
+ */
+typedef struct
+{
+  const char*        _local_ipv4;
+  uint16_t           _local_port;
+  const char*        _remote_ipv4;
+  uint16_t           _remote_port;
+  struct sockaddr_in _local_sa;
+  struct sockaddr_in _remote_sa;
+} spawn_and_connect_args;
+
+/**
+ * spawn_and_connect_cb
+ * 
+ * @summary
+ *   Callback wrapper for `spawn_and_connect`,
+ *     to expose as void*(*func_name_here)(void*) for pthread.
+ *
+ * @return
+ *   NULL, but provides printout for errors in `spawn_and_connect`.
+ *
+ */
+void* spawn_and_connect_cb(void*);
+
+#endif // SPAWN_H
+
