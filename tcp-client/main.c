3bbd1a8032c3e2947b8002bf2be06733bd06a8f9
Mehdy Faik <faik.mehdy@gmail.com>
2022-02-25 01:05:43 -0700

    implemented the parallel spawn-and-connect method, but it doesn not quite work the way i need...

--- tcp-client/main.c
+++ tcp-client/main.c
@@ -8,10 +8,16 @@
 #include <string.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <pthread.h>
 
 #include "main.h"
 
 int32_t gl_verbose_error = 1;
+
+pthread_mutex_t gl_lock_socklib;
+// pthread_mutex_t gl_lock_socket;
+// pthread_mutex_t gl_lock_bind;
+// pthread_mutex_t gl_lock_connect;
 
 void seed_random()
 {
@@ -99,6 +105,109 @@ int32_t spawn_and_connect
   return rtn_local_fd;
 }
 
+int32_t spawn_and_connect_lock
+  ( const char* arg_local_ipv4
+  , uint16_t arg_local_port
+  , const char* arg_remote_ipv4
+  , uint16_t arg_remote_port
+  , struct sockaddr_in* rtn_local_sa
+  , struct sockaddr_in* rtn_remote_sa
+  )
+{
+  // arg check.
+  //
+  if ( (rtn_local_sa == NULL) || (rtn_remote_sa == NULL) ) { return -1; }
+
+  // lock global.
+  pthread_mutex_lock(&gl_lock_socklib);
+
+  // // lock socket().
+  // pthread_mutex_lock(&gl_lock_socket);
+
+  // socket().
+  // 
+  int32_t rtn_local_fd = socket(AF_INET, SOCK_STREAM, 0);
+  if (rtn_local_fd < 0)
+  {
+    if (gl_verbose_error)
+    {
+      printf("socket: error '%d', errno '%d' ('%s')\n"
+      , rtn_local_fd
+      , errno
+      , strerror(errno)
+      );
+    }
+    return rtn_local_fd;
+  }
+
+  // setsockopt() to allow reuse.
+  // 
+  int32_t reuse_enable = 1;
+  setsockopt(rtn_local_fd, SOL_SOCKET, SO_REUSEADDR, &reuse_enable, sizeof(reuse_enable));
+  setsockopt(rtn_local_fd, SOL_SOCKET, SO_REUSEPORT, &reuse_enable, sizeof(reuse_enable));
+
+  // // unlock socket().
+  // pthread_mutex_unlock(&gl_lock_socket);
+
+  // // lock bind().
+  // pthread_mutex_lock(&gl_lock_bind);
+
+  // bind() to the local.
+  //
+  rtn_local_sa->sin_family = AF_INET;
+  inet_pton( AF_INET, arg_local_ipv4, &(rtn_local_sa->sin_addr.s_addr) );
+  rtn_local_sa->sin_port = htons(arg_local_port);
+
+  int32_t error_bind = bind(rtn_local_fd, (struct sockaddr*)(rtn_local_sa), sizeof(*rtn_local_sa));
+  if (error_bind < 0)
+  {
+    if (gl_verbose_error)
+    {
+      printf("bind: error '%d', errno '%d' ('%s')\n"
+      , error_bind
+      , errno
+      , strerror(errno)
+      );
+    }
+    return error_bind;
+  }
+
+  // // unlock bind().
+  // pthread_mutex_unlock(&gl_lock_bind);
+
+  // // lock connect().
+  // pthread_mutex_lock(&gl_lock_connect);
+
+  // connect() to the remote.
+  //
+  rtn_remote_sa->sin_family = AF_INET;
+  inet_pton( AF_INET, arg_remote_ipv4, &(rtn_remote_sa->sin_addr.s_addr) );
+  rtn_remote_sa->sin_port = htons(arg_remote_port);
+
+  int32_t error_connect = connect(rtn_local_fd, (struct sockaddr*)(rtn_remote_sa), sizeof(*rtn_remote_sa));
+  if (error_connect < 0)
+  {
+    if (gl_verbose_error)
+    {
+      printf("connect: error '%d', errno '%d' ('%s')\n"
+      , error_connect
+      , errno
+      , strerror(errno)
+      );
+    }
+    return error_connect;
+  }
+
+  // // unlock connect().
+  // pthread_mutex_unlock(&gl_lock_connect);
+
+  // unlock global.
+  pthread_mutex_unlock(&gl_lock_socklib);
+
+  // ret.
+  return rtn_local_fd;
+}
+
 int32_t spawn_serial
   ( uint64_t arg_num_spawn
   , const char* arg_local_ipv4
@@ -108,7 +217,6 @@ int32_t spawn_serial
 {
   // vars.
   uint64_t n_sp = 0;
-  uint16_t local_port = 0;
   int32_t rtn_spawn_error = 0;
   struct sockaddr_in local_sa;
   struct sockaddr_in remote_sa;
@@ -116,8 +224,14 @@ int32_t spawn_serial
   // spawn in serial.
   for (n_sp = 0; n_sp < arg_num_spawn; n_sp++)
   {
-    rtn_spawn_error = spawn_and_connect(
-      arg_local_ipv4, 10000 + n_sp, arg_remote_ipv4, arg_remote_port, &local_sa, &remote_sa);
+    rtn_spawn_error = spawn_and_connect
+      ( arg_local_ipv4
+      , (n_sp % 50000) + 10000 // [0, 49999] => [10000, 59999]
+      , arg_remote_ipv4
+      , arg_remote_port
+      , &local_sa
+      , &remote_sa
+      );
 
     if (rtn_spawn_error < 0)
     {
@@ -132,13 +246,103 @@ int32_t spawn_serial
   return rtn_spawn_error;
 }
 
+void* spawn_and_connect_cb(void* p_sac_args)
+{
+  // marshal args.
+  spawn_and_connect_args* sac_args = (spawn_and_connect_args*)p_sac_args;
+
+  // call.
+  int32_t error_sac = spawn_and_connect_lock
+    ( sac_args->_local_ipv4
+    , sac_args->_local_port
+    , sac_args->_remote_ipv4
+    , sac_args->_remote_port
+    , &( sac_args->_local_sa )
+    , &( sac_args->_remote_sa )
+    );
+
+  if (error_sac < 0)
+  {
+    if (gl_verbose_error)
+    {
+      printf("spawn_and_connect: error '%d'\n", error_sac);
+    }
+  }
+
+  // ret.
+  return NULL;
+}
+
+int32_t spawn_parallel
+  ( uint64_t arg_num_spawn
+  , const char* arg_local_ipv4
+  , const char* arg_remote_ipv4
+  , uint16_t arg_remote_port
+  , bool arg_should_join
+  )
+{
+  // vars.
+  uint64_t n_sp = 0;
+
+  int32_t error_pcreate = 0;
+  pthread_t cur_pthread;
+  pthread_attr_t cur_pattr;
+  pthread_attr_init(&cur_pattr); // default
+
+  spawn_and_connect_args sac_args;
+    sac_args._local_ipv4 = arg_local_ipv4;
+    sac_args._remote_ipv4 = arg_remote_ipv4;
+    sac_args._remote_port = arg_remote_port;
+
+  pthread_t* all_pthreads = NULL;
+  if (arg_should_join)
+    { all_pthreads = malloc(sizeof(pthread_t) * arg_num_spawn); }
+
+  // spawn in parallel.
+  //
+  for (n_sp = 0; n_sp < arg_num_spawn; n_sp++)
+  {
+    sac_args._local_port = (n_sp % 50000) + 10000; // [0, 49999] => [10000, 59999]
+
+    error_pcreate = pthread_create
+      ( &cur_pthread
+      , &cur_pattr
+      , &spawn_and_connect_cb
+      , (void*)(&sac_args)
+      );
+
+    if (error_pcreate < 0)
+    {
+      if (gl_verbose_error)
+      {
+        printf("pthread_create: error '%d'\n", error_pcreate);
+      }
+    }
+
+    if (arg_should_join && all_pthreads != NULL)
+      { all_pthreads[n_sp] = cur_pthread; }
+  }
+
+  // optionally, ensure all threads complete.
+  //
+  if (arg_should_join && all_pthreads != NULL)
+  {
+    for (n_sp = 0; n_sp < arg_num_spawn; n_sp++)
+      { pthread_join(all_pthreads[n_sp], NULL); }
+    free(all_pthreads);
+  }
+
+  // ret.
+  return error_pcreate;
+}
+
 int main(int argc, char** argv)
 {
   // seed random.
   seed_random();
 
-  struct sockaddr_in local_sa;
-  struct sockaddr_in remote_sa;
+  // struct sockaddr_in local_sa;
+  // struct sockaddr_in remote_sa;
   // int32_t local_fd = spawn_and_connect(
   //   "127.0.0.1", (rand() % 10000) + 10000, "127.0.0.1", 44444, &local_sa, &remote_sa);
   // int32_t local_fd = spawn_and_connect(
@@ -149,7 +353,10 @@ int main(int argc, char** argv)
   // 157.240.28.35:443 is `facebook.com`
   // 
   // spawn_serial(10, "127.0.0.1", "127.0.0.1", 44444);
-  spawn_serial(100, "192.168.0.10", "157.240.28.35", 443);
+  // spawn_serial(100, "192.168.0.10", "157.240.28.35", 443);
+
+  spawn_parallel(100, "127.0.0.1", "127.0.0.1", 44444, true);
+  // spawn_parallel(100, "192.168.0.10", "157.240.28.35", 443);
 
   // ret.
   return 0;
--- tcp-client/main.h
+++ tcp-client/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdbool.h>
 #include <stdint.h>
 
 #include <sys/types.h>
@@ -41,13 +42,32 @@ int32_t spawn_and_connect
   );
 
 /**
+ * spawn_and_connect_lock
+ *
+ * @summary
+ *   `spawn_and_connect`, but places a lock around socket calls.
+ *
+ * @return
+ *   ref. `spawn_and_connect`.
+ *
+ */
+int32_t spawn_and_connect_lock
+  ( const char* arg_local_ipv4
+  , uint16_t arg_local_port
+  , const char* arg_remote_ipv4
+  , uint16_t arg_remote_port
+  , struct sockaddr_in* rtn_local_sa
+  , struct sockaddr_in* rtn_remote_sa
+  );
+
+/**
  * spawn_serial
  *
  * @summary
  *   Calls `spawn_and_connect` for 'arg_num_spawn' TCP clients, in serial.
- *   Each TCP client is bound to 'arg_local_ipv4' on a random port,
+ *   Each TCP client is bound to 'arg_local_ipv4' on an internally-calculated port,
  *     and connects to 'arg_remote_ipv4':'arg_remote_port'.
- *   Returns early if any `spawn_and_connect` fails.
+ *   Reports, but does not return-early, if any `spawn_and_connect` fails.
  *
  * @return
  *   The return value of the final `spawn_and_connect` call.
@@ -57,6 +77,55 @@ int32_t spawn_serial
   , const char* arg_local_ipv4
   , const char* arg_remote_ipv4
   , uint16_t arg_remote_port
+  );
+
+/**
+ * spawn_and_connect_args
+ * @summary
+ *   Represents wrapper for arguments to `spawn_and_connect`.
+ */
+typedef struct
+{
+  const char*        _local_ipv4;
+  uint16_t           _local_port;
+  const char*        _remote_ipv4;
+  uint16_t           _remote_port;
+  struct sockaddr_in _local_sa;
+  struct sockaddr_in _remote_sa;
+} spawn_and_connect_args;
+
+/**
+ * spawn_and_connect_cb
+ * 
+ * @summary
+ *   Callback wrapper for `spawn_and_connect`,
+ *     to expose as void*(*func_name_here)(void*) for pthread.
+ *
+ * @return
+ *   NULL, but provides printout for errors in `spawn_and_connect`.
+ *
+ */
+void* spawn_and_connect_cb(void*);
+
+/**
+ * spawn_parallel
+ *
+ * @summary
+ *   Calls `spawn_and_connect` for 'arg_num_spawn' TCP clients, each on their own thread.
+ *   Each TCP client is bound to 'arg_local_ipv4' on an internally-calculated port,
+ *     and connects to 'arg_remote_ipv4':'arg_remote_port'.
+ *   Reports, but does not return-early, if any `spawn_and_connect` fails.
+ *
+ * @return
+ *   The return value of the final `pthread_create` call.
+ *
+ */
+int32_t spawn_parallel
+  ( uint64_t arg_num_spawn
+  , const char* arg_local_ipv4
+  , const char* arg_remote_ipv4
+  , uint16_t arg_remote_port
+  , bool arg_should_join
   );
 
 #endif // MAIN_H
